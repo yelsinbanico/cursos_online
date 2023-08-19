6d68630c54c8f838d00b46dc6a2a480f62499461
Yelsin Bañico <neil.banico.27@unsch.edu.pe>
2023-08-19 15:14:31 -0500

    agregando login

--- Js/main.js
+++ Js/main.js
@@ -5,3 +5,7 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards',{delay:500});
 ScrollReveal().reveal('.cards-banner-one',{delay:500});
 ScrollReveal().reveal('.cards-banner-two',{delay:500});
+
+document.querySelector('.menu-btn').addEventListener('click', ()=>{
+    document.querySelector('.login').classList.toggle('show');
+})
